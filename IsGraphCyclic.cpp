3e72eb98c41e36c35a080d529f15ed786737c3fa
vedmandal <151810723+vedmandal@users.noreply.github.com>
2026-01-04 13:49:41 +0530

    is graph cyclic

---
+++ IsGraphCyclic.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<queue>
+
+using namespace std;
+
+
+class Edge{
+    public:
+    int src;
+    int nbr;
+    int w;
+    Edge(int src,int nbr,int w){
+        this->src=src;
+        this->nbr=nbr;
+        this->w=w;
+    }
+};
+
+class pairs{
+    public:
+    int v;
+    string path;
+    pairs(int v,string path){
+        this->v=v;
+        this->path=path;
+    }
+};
+
+bool isCyclic( vector<vector<Edge>>&graph,vector<bool>&vis,int src){
+
+    queue<pairs>q;
+    q.push({src,to_string(src)+" "});
+
+    while(!q.empty()){
+        pairs rem=q.front();
+
+        q.pop();
+
+        if(vis[rem.v]==true){
+            return true;
+        }
+        vis[rem.v]=true;
+
+
+        for(auto edge:graph[rem.v]){
+            if(vis[edge.nbr]==false){
+                q.push({edge.nbr,rem.path+to_string(edge.nbr)});
+            }
+        }
+
+    }
+    return false;
+
+}
+
+int main(){
+    int v,e;
+    cout<<"Enter vetrx and edge"<<endl;
+    cin>>v>>e;
+
+    vector<vector<Edge>>graph(v);
+
+    for(int i=0;i<e;i++){
+        int src,nbr,w;
+        cout<<"enter edge (src nbr w)"<<endl;
+        cin>>src>>nbr>>w;
+        graph[src].push_back({src,nbr,w});
+        graph[nbr].push_back({nbr,src,w});
+    }
+
+    vector<bool>vis(v,false);
+
+    for(int i=0;i<v;i++){
+        if(vis[i]==false){
+            if(isCyclic(graph,vis, i)==true){
+                cout<<"cycle is present"<<endl;
+                return;
+            }
+        }
+    }
+    cout<<"Cycle is not present"<<endl;
+}
